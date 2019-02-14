72177c499a66d201d4a5be6819bcfeb2faee5aa5
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-02-14 13:50:28 +0300

    Dosya yolları düzeltildi.

--- TeknikServis.Web.UI/Areas/Admin/BundleConfig.cs
+++ TeknikServis.Web.UI/Areas/Admin/BundleConfig.cs
@@ -12,39 +12,39 @@ namespace TeknikServis.Web.UI.Areas.Admin
         {
             ///Areas/Admin
             // Vendor scripts
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/Areas/Admin/bundles/jquery").Include(
                         "~/Areas/Admin/Scripts/jquery-2.1.1.min.js"));
 
             // jQuery Validation
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            "~/Scripts/jquery.validate.min.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/bundles/jqueryval").Include(
+            "~/Areas/Admin/Scripts/jquery.validate.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/bundles/bootstrap").Include(
+                      "~/Areas/Admin/Scripts/bootstrap.min.js"));
 
             // Inspinia script
-            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
-                      "~/Scripts/app/inspinia.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/bundles/inspinia").Include(
+                      "~/Areas/Admin/Scripts/app/inspinia.js"));
 
             // SlimScroll
-            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
-                      "~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/plugins/slimScroll").Include(
+                      "~/Areas/Admin/Scripts/plugins/slimScroll/jquery.slimscroll.min.js"));
 
             // jQuery plugins
-            bundles.Add(new ScriptBundle("~/plugins/metsiMenu").Include(
-                      "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/plugins/metsiMenu").Include(
+                      "~/Areas/Admin/Scripts/plugins/metisMenu/metisMenu.min.js"));
 
-            bundles.Add(new ScriptBundle("~/plugins/pace").Include(
-                      "~/Scripts/plugins/pace/pace.min.js"));
+            bundles.Add(new ScriptBundle("~/Areas/Admin/plugins/pace").Include(
+                      "~/Areas/Admin/Scripts/plugins/pace/pace.min.js"));
 
             // CSS style (bootstrap/inspinia)
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Areas/Admin/Content/css").Include(
                       "~/Areas/Admin/Content/bootstrap.min.css",
                       "~/Areas/Admin/Content/animate.css",
                       "~/Areas/Admin/Content/style.css"));
 
             // Font Awesome icons
-            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+            bundles.Add(new StyleBundle("~/Areas/Admin/font-awesome/css").Include(
                       "~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
 
         }
