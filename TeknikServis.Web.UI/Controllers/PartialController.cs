145c61e9b0a0b61315447a89e3ff5a114b6fd4a9
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-02-14 11:15:41 +0300

    Template giydirme işlemi başarıyla tamamlandı.

---
+++ TeknikServis.Web.UI/Controllers/PartialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeknikServis.Web.UI.Controllers
+{
+    public class PartialController : Controller
+    {
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView("Partial/_NavbarPartial");
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView("Partial/_FooterPartial");
+        }
+    }
+}
