70e49c87dfcbd8bd5496abbda77f8f0210100102
hfatih15 <fatih.hancer95@gmail.com>
2019-02-14 12:36:17 +0300

    Admin Area Olusturuldu.

---
+++ TeknikServis.Web.UI/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace TeknikServis.Web.UI.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+            RegisterBundle();
+        }
+
+        private void RegisterBundle()
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
