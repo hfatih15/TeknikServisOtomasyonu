af8e3c7b129db5881cb3a23feaff3fe7f0b518bb
kubilay yazı <kubilayyazi@hotmail.com>
2019-02-21 23:02:16 +0300

    Ariza Kayıt İşlemi Başarıyla Tamamlanmıştır.

--- TeknikServis.Models/ViewModels/ArizaViewModel.cs
+++ TeknikServis.Models/ViewModels/ArizaViewModel.cs
@@ -81,9 +81,14 @@ namespace TeknikServis.Models.ViewModels
             [DisplayName("Şehir Adı")]
             public SehirAdi SehirAdi { get; set; }
 
-           
+        [DisplayName("Oluşturma Tarihi")]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [DisplayName("Güncelleme Tarihi")]
+        public DateTime? UpdatedDate { get; set; }
 
-       
+
+
+
 
     }
 }
--- TeknikServis.Web.UI/Controllers/MusteriController.cs
+++ TeknikServis.Web.UI/Controllers/MusteriController.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using TeknikServis.BLL.Helpers;
+using TeknikServis.BLL.Repository;
+using TeknikServis.Models.Entities;
+using TeknikServis.Models.ViewModels;
+using static TeknikServis.BLL.Identity.MemberShipTools;
 
 namespace TeknikServis.Web.UI.Controllers
 {
@@ -14,6 +22,86 @@ namespace TeknikServis.Web.UI.Controllers
             return View();
         }
 
+
+        public ActionResult Ekle (ArizaViewModel model)
+        {
+            try
+            {
+                var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+                var user = NewUserManager().FindById(id);
+                // arıza fotoğrafı
+                if (model.UrunFotografi != null &&
+                   model.UrunFotografi.ContentLength > 0)
+                {
+                    var file = model.UrunFotografi;
+                    string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                    string extName = Path.GetExtension(file.FileName);
+                    fileName = StringHelpers.UrlFormatConverter(fileName);
+                    fileName += StringHelpers.GetCode();
+                    var klasoryolu = Server.MapPath("~/ArizaFoto/");
+                    var dosyayolu = Server.MapPath("~/ArizaFoto/") + fileName + extName;
+
+                    if (!Directory.Exists(klasoryolu))
+                        Directory.CreateDirectory(klasoryolu);
+                    file.SaveAs(dosyayolu);
+
+                    WebImage img = new WebImage(dosyayolu);
+                    img.Resize(250, 250, false);
+                    img.AddTextWatermark("FK");
+                    img.Save(dosyayolu);
+                    var oldPath = model.UrunResmi;
+                   model.UrunResmi = "/ArizaFoto/" + fileName + extName;
+
+                    System.IO.File.Delete(Server.MapPath(oldPath));
+                }
+
+                // arıza fotoğrafı
+                if (model.UrunFaturaFotografi != null &&
+                   model.UrunFaturaFotografi.ContentLength > 0)
+                {
+                    var file = model.UrunFaturaFotografi;
+                    string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                    string extName = Path.GetExtension(file.FileName);
+                    fileName = StringHelpers.UrlFormatConverter(fileName);
+                    fileName += StringHelpers.GetCode();
+                    var klasoryolu = Server.MapPath("~/FaturaFoto/");
+                    var dosyayolu = Server.MapPath("~/FaturaFoto/") + fileName + extName;
+
+                    if (!Directory.Exists(klasoryolu))
+                        Directory.CreateDirectory(klasoryolu);
+                    file.SaveAs(dosyayolu);
+
+                    WebImage img = new WebImage(dosyayolu);
+                    img.Resize(250, 250, false);
+                    img.AddTextWatermark("FK");
+                    img.Save(dosyayolu);
+                    var oldPath = model.FaturaResmi;
+                    model.FaturaResmi = "/FaturaFoto/" + fileName + extName;
+
+                    System.IO.File.Delete(Server.MapPath(oldPath));
+                }
+
+                new ArizaRepo().Insert(new Ariza() {
+                     UrunTipi=model.UrunTipi,
+                     UrunAdi=model.UrunAdi,
+                     MusteriYorumu=model.MusteriYorumu,
+                      UrunResmi=model.UrunResmi,
+                      FaturaResmi=model.FaturaResmi,
+                      SehirAdi=model.SehirAdi,
+                      Adres=model.Adres,
+                      GarantiDurumu=model.GarantiDurumu,
+                      MusteriId=id   
+                });
+                
+  
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("ArizaKayit","Musteri");
+        }
 
 
     }
