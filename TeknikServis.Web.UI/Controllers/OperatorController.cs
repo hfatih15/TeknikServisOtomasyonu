a74d88d93154b79e7ffcd18a5ee9d0815e4d63f2
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-02-22 11:19:18 +0300

    Arıza Listesi Oluşturuldu.

---
+++ TeknikServis.Web.UI/Controllers/OperatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeknikServis.BLL.Repository;
+using TeknikServis.Models.Entities;
+
+namespace TeknikServis.Web.UI.Controllers
+{
+    public class OperatorController : Controller
+    {
+        // GET: Operator
+        public ActionResult Index()
+        {
+            return View(new ArizaRepo().GetAll());
+        }
+    }
+}
